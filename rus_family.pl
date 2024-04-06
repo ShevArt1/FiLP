f877af1dd628dfd77291bd4e6ef5407ec63ca353
ShevArt1 <151970693+ShevArt1@users.noreply.github.com>
2024-04-06 14:39:14 +0300

    ЛР 1, задания 1-3, исправлены комментарии к предикатам

--- rus_family.pl
+++ rus_family.pl
@@ -54,32 +54,32 @@ women:-woman(X),write(X),nl,fail.
 % children(+X) - all children of X
 children(X):-parent(X,Y),write(Y),nl,fail.
 
-% mother(+X, +Y) - is X  a mother of Y
+% mother(?X, +Y) - is X  a mother of Y
 mother(X,Y):-parent(X,Y),woman(X),nl.
 % mother(+X) - a mother of X
 mother(X):-mother(Y,X),write(Y),nl.
 
-% brother(+X, +Y) - is X  a brother of Y
+% brother(?X, +Y) - is X  a brother of Y
 brother(X,Y):-parent(A,X),parent(A,Y),X\=Y,man(X),man(A),nl.
 % brothers(+X) - all brothers of X
 brothers(X):-brother(Y,X),write(Y),nl,fail.
 
-% b_s(+X, +Y) - is X a brother or sister of Y 
+% b_s(?X, +Y) - is X a brother or sister of Y 
 b_s(X,Y):-parent(A,X),parent(A,Y),man(A),X\=Y,nl.
 % b_s(+X) - all brothers and sisters of X
 b_s(X):-b_s(Y,X),write(Y),nl,fail.
 
-% daughter(+X, +Y) - is X a daughter of Y
+% daughter(?X, +Y) - is X a daughter of Y
 daughter(X,Y):-parent(Y,X),woman(X),nl.
 % daughter(+X) - all daughters of X
-daughter(X):-parent(X,Y),woman(Y),write(Y),nl,fail.
+daughter(X):-daughter(Y,X),write(Y),nl,fail.
 
-% wife(+X, +Y) - is X a wife of Y
+% wife(?X, +Y) - is X a wife of Y
 wife(X,Y):-parent(X,A),parent(Y,A),woman(X),X\=Y,nl.
 % wife(+X) - wife of X
 wife(X):-wife(Y,X),write(Y),nl.
 
-% grandson(+X, +Y) - is X a grandson of Y
+% grandson(?X, +Y) - is X a grandson of Y
 grandson(X,Y):-parent(Y,A),parent(A,X),man(X),nl.
 % grandsons(+X) - all grandsons of X
 grandsons(X):-grandson(Y,X),write(Y),nl,fail.
@@ -88,7 +88,7 @@ grandsons(X):-grandson(Y,X),write(Y),nl,fail.
 gmas(X,Y):-grandson(X,Y),woman(Y),nl.
 gmas(X,Y):-grandson(Y,X),woman(X),nl.
 
-% uncle(+X, +Y) - is X an uncle of Y
+% uncle(?X, +Y) - is X an uncle of Y
 uncle(X,Y):-parent(A,Y),brother(X,A),nl.
 % uncles(+X) - all uncles of X
 uncles(X):-uncle(Y,X),write(Y),nl,fail.
